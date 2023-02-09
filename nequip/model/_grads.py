d5c23eb89cb80f67229f1882f4b1079d0d11063b
akvyj <akvyj@login001.ianvs.xd.uni-halle.de>
2023-02-09 09:08:55 +0100

    Hessian function in grads.py

--- nequip/model/_grads.py
+++ nequip/model/_grads.py
@@ -1,4 +1,4 @@
-from nequip.nn import GraphModuleMixin, GradientOutput
+from nequip.nn import GraphModuleMixin, GradientOutput, HessianOutput
 from nequip.nn import PartialForceOutput as PartialForceOutputModule
 from nequip.nn import StressOutput as StressOutputModule
 from nequip.data import AtomicDataDict
@@ -57,19 +57,19 @@ def StressForceOutput(model: GraphModuleMixin) -> GradientOutput:
         raise ValueError("This model already has force or stress outputs.")
     return StressOutputModule(func=model)
 
-def ForceConstantOutput(model: GraphModuleMixin) -> GradientOutput:
+def ForceConstantOutput(model: GraphModuleMixin) -> HessianOutput:
     r"""Add force constants to a model that predicts energy and forces.
     Args:
-        model: the energy model to wrap. Must have ``AtomicDataDict.FORCE_KEY`` as an output.
+        model: the energy model to wrap. Must have ``AtomicDataDict.HESSIAN_KEY`` as an output.
     Returns:
-        A ``GradientOutput`` wrapping ``model``.
+        A ``HessianOutput`` wrapping ``model``.
     """
-    if AtomicDataDict.FORCE_CONSTANTS_KEY in model.irreps_out:
+    if AtomicDataDict.HESSIAN_KEY in model.irreps_out:
         raise ValueError("This model already has force constant outputs.")
-    return Gradient_GradientOutput(
+    return HessianOutput(
         func=model,
-        of=AtomicDataDict.FORCE_KEY,
+        of=AtomicDataDict.TOTAL_ENERGY_KEY,
         wrt=AtomicDataDict.POSITIONS_KEY,
-        out_field=AtomicDataDict.FORCE_CONSTANTS_KEY,
-        sign=-1,  # force is the negative gradient
+        out_field=AtomicDataDict.HESSIAN_KEY,
+        #sign=-1,  # force is the negative gradient
     )
